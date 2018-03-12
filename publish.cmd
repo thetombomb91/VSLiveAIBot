e45886d78b877cf6cc587eef2a3f9b575a82df28
Thomas Desmond <thomasdes533@gmail.com>
2018-03-12 10:38:56 -0700

    Initial Commit

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=vsliveaibotthomas-Web-Deploy.pubxml -p:Password=X4df4a39sh8sPgT29uov9y1gJJWZk33allCQpX4N4b19ebcHN99W5rz4llYW
+
